aa5d876a0684d01ef37eac29e29933f93124c067
Arlo O'Keeffe <mailtoarlo@gmail.com>
2013-09-28 13:58:15 +0200

    Add redirect.php

---
+++ redirect.php
@@ -0,0 +1,19 @@
+---
+layout: nil
+---
+<?php
+
+$aRedirects = array(
+    'home' => '{{site.url}}',
+);
+
+$sPath = $_GET['path'];
+
+if (array_key_exists($sPath, $aRedirects)) {
+	$sRedirect = $aRedirects[$sPath];
+	header('HTTP/1.1 301 Moved Permanently');
+	header('Location: ' . $sRedirect);
+} else {
+	header("HTTP/1.1 404 Not Found");
+	include '404.html';
+}
